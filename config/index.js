b6bc1d94ee06760d2aaf9840e90d70cdc7ac9530
蒋亚光 <jiangyaguang@192.168.0.12>
2018-11-18 19:35:01 +0800

    修改port

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
         proxyTable: {},
 
         // Various Dev Server settings
-        host: process.env.NODE_ENV == 'production' ? "172.19.56.164" : 'localhost', // can be overwritten by process.env.HOST
+        host: "172.19.56.164", //'production' ? "172.19.56.164" : 'localhost', // can be overwritten by process.env.HOST
         port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
         autoOpenBrowser: false,
         errorOverlay: true,
